7cf4f89d5e61e6014ca2a7039cb6f7e7dd803d31
Juan Vanegas <jandvanegas@gmail.com>
2022-04-13 12:26:36 +0200

    feat(navbar, content): added navbar and draft of content.

--- biglab1/src/App.js
+++ biglab1/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import {AppNavbar} from './components/LibraryComponents'
+import Col from 'react-bootstrap/Col';
+import dayjs from 'dayjs'
+import { AppNavbar, SideBar, Films } from './components/LibraryComponents'
+
+const fakeFilms = [
+  { id: 1, name: 'Pulp Fiction', favorite: true, watchDate: dayjs('2022-03-10'), score: 5 },
+  { id: 2, name: '21 Grams', favorite: true, watchDate: dayjs('2022-03-17'), score: 4 },
+  { id: 3, name: 'Star Wars', favorite: false },
+  { id: 4, name: 'Matrix', favorite: false, },
+  { id: 5, name: 'Shrek', favorite: false, watchDate: dayjs('2022-03-21'), score: 3 },
+];
+
+const filterTitle = 'Filter:All'
 
 function App() {
   return (
     <Container className="App">
       <Row>
-        <AppNavbar/>
+        <AppNavbar />
+      </Row>
+      <Row>
+        <Col xs={4}>
+          <SideBar/>
+        </Col>
+        <Col xs={8}>
+          <Films films={fakeFilms} title={filterTitle} />
+        </Col>
       </Row>
     </Container>
   );
--- biglab1/src/components/LibraryComponents.js
+++ biglab1/src/components/LibraryComponents.js
@@ -3,48 +3,99 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import Navbar from 'react-bootstrap/Navbar'
 import FormControl from 'react-bootstrap/FormControl'
 import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Table from 'react-bootstrap/Table'
 
 function AppNavbar() {
   return (
-    <>
-      <div className="d-flex flex-row justify-content-between">
-        <Navbar bg="primary" variant="dark" expand="md" fixed="top">
-          <div className="col p-2">
-            <div className="d-flex justify-content-start">
-              <Navbar.Brand>
-                <div className="col">
-                  <i className='bi bi-collection-play text-white'> </i>
-                  Film Library
-                </div>
-              </Navbar.Brand>
-            </div>
-          </div>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <div className="col">
-              <Form inline>
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  arial-label="Search"
-                  size='sm' />
-              </Form>
-            </div>
-            <div className="col">
-              <div className="d-flex justify-content-end">
-                <Navbar.Text>
-                  <div className='col p-2'>
-                    <i className='bi bi-person-circle text-white'> </i>
-                  </div>
-                </Navbar.Text>
-              </div>
-            </div>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-
-    </>
+    <Navbar bg="primary" variant="dark" expand="md" fixed="top">
+      <Col><FilmBrand /></Col>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Col><FilmSearchForm /></Col>
+        <Col><FilmUser /></Col>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
-export { AppNavbar }
+function FilmUser() {
+  return <Navbar.Text className='d-flex justify-content-end'>
+    <i className='bi bi-person-circle text-white'> </i>
+  </Navbar.Text>
+}
+
+function FilmSearchForm() {
+  return <Form inline>
+    <FormControl
+      type="search"
+      placeholder="Search"
+      arial-label="Search"
+      size='sm' />
+  </Form>
+}
+
+function FilmBrand() {
+  return <Navbar.Brand className='d-flex justify-content-start'>
+    <i className='bi bi-collection-play text-white'> </i>
+    Film Library
+  </Navbar.Brand>
+}
+
+function SideBar() {
+  return <Container>
+    <Row className='p-2 bg-primary text-white border-bottom'>All</Row>
+    <Row className='p-2 border-bottom'>Favorites</Row>
+    <Row className='p-2 border-bottom'>Best Rated</Row>
+    <Row className='p-2 border-bottom'>Seen Last Month</Row>
+    <Row className='p-2 border-bottom'>Unseen</Row>
+  </Container>
+}
+
+function Films(props) {
+  return <Container>
+    <Row className='d-flex flex-row justify-content-start'>
+      <Col xs={2}>
+        <h1 className='text-dark'>{props.title}</h1>
+      </Col>
+    </Row>
+    <Row>
+      <FilmsTable films={props.films} />
+    </Row>
+  </Container>
+
+}
+
+function FilmsTable(props) {
+  return <Table striped>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Favorite</th>
+        <th>Watch</th>
+        <th>Score</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        props.films.map((film) => <FilmRow film={film} key={film.id} />)
+      }
+    </tbody>
+  </Table>
+}
+
+function FilmRow(props) {
+  return <>
+    <tr>
+      <td>{props.film.name} </td>
+      <td>{props.film.favorite} </td>
+      <td>{props.film.watchedDate} </td>
+      <td>{props.film.score} </td>
+    </tr>
+  </>
+}
+
+
+export { AppNavbar, SideBar, Films }
